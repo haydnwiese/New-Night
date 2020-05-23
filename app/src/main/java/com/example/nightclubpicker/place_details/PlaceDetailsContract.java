eaef83e48a49621e60b0b30cc2f98e44504d6844
haydnwiese <45501692+haydnwiese@users.noreply.github.com>
2020-05-22 22:16:47 -0400

    Created base layout to convert PlaceDetails to MVP

---
+++ app/src/main/java/com/example/nightclubpicker/place_details/PlaceDetailsContract.java
@@ -0,0 +1,13 @@
+package com.example.nightclubpicker.place_details;
+
+import com.example.nightclubpicker.common.BasePresenter;
+
+public interface PlaceDetailsContract {
+    interface View {
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+    }
+}
---
+++ app/src/main/java/com/example/nightclubpicker/place_details/PlaceDetailsPresenter.java
@@ -0,0 +1,10 @@
+package com.example.nightclubpicker.place_details;
+
+public class PlaceDetailsPresenter implements PlaceDetailsContract.Presenter {
+
+
+    @Override
+    public void onViewCreated() {
+
+    }
+}
